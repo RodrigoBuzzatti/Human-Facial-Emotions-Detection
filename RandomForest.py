7c78d9d89293f830389188a7e5a8b05a88f9b5cb
RodrigoBuzzatti <rodrigo.buzzatti@gmail.com>
2025-05-12 00:04:03 -0300

    Inclusao do modelo RandomForest

--- RandomForest.py
+++ RandomForest.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 # Função para carregar imagens de um diretório e redimensionar
@@ -39,27 +41,6 @@ print('Shape do Treino: ',X_train.shape)
 # Carregar imagens de teste
 X_test, y_test = load_folder(test_folder, img_size)
 print('Shape do Teste: ',X_test.shape)
-
-"""
-# Função para detectar imagens corrompidas
-def detect_corrupted_images(folder):
-    corrupted_files = []
-    for filename in os.listdir(folder):
-        if filename.endswith(".jpg"):
-            try:
-                img = Image.open(os.path.join(folder, filename))
-                img.verify()  # Verificar a integridade do arquivo
-            except (IOError, SyntaxError) as e:
-                corrupted_files.append(filename)
-    return corrupted_files
-
-# Detectar imagens corrompidas
-corrupted_train_images = detect_corrupted_images(train_folder)
-print(f"Imagens corrompidas no conjunto de treino: {corrupted_train_images}")
-
-corrupted_submission_images = detect_corrupted_images(submission_folder)
-print(f"Imagens corrompidas no conjunto de submissão: {corrupted_submission_images}")
-"""
 
 import random
 
@@ -135,41 +116,69 @@ X_test_flat = X_test.reshape((X_test.shape[0], -1))
 print('Apos o Flatten, shape do X_trian: ',X_train_flat.shape)
 print('Apos o Flatten, shape do X_test: ',X_test_flat.shape)
 
-from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score, cohen_kappa_score, f1_score, confusion_matrix
+import seaborn as sns
 
-n_components = 500
-pca = PCA(n_components=n_components)
-X_train_pca = pca.fit_transform(X_train_flat)
-X_test_pca = pca.transform(X_test_flat)
-
-# Plotar variância acumulada
-print('Resultado da Variancia acumulada do PCA')
-explained_variance_ratio = pca.explained_variance_ratio_
-cumulative_variance = np.cumsum(explained_variance_ratio)
-
-plt.figure(figsize=(10, 6))
-plt.plot(range(1, n_components + 1), cumulative_variance, marker='o', linestyle='--')
-plt.xlabel('Número de Componentes')
-plt.ylabel('Variância Acumulada')
-plt.title('Variância Acumulada Explicada pelos Componentes Principais')
-plt.annotate("{:.2f}".format(cumulative_variance[-1]), xy=(n_components+1, cumulative_variance[-1]), color='red')
-plt.grid(True)
-plt.show()
+from skimage.feature import hog
+# Extração de características utilizando HOG
+def extract_hog_features(images):
+    hog_features = []
+    hog_images = []
+    for image in images:
+        features, img = hog(image, orientations=9,      # o número de direções angulares distintas para as quais os gradientes são calculados
+                              pixels_per_cell=(8, 8),   # tamanho (em pixels) de cada célula na qual a imagem é dividida para calcular o histograma de gradientes orientados
+                              cells_per_block=(2, 2),   # número de células em cada bloco. Os blocos são usados para normalizar os histogramas de gradientes dentro de células, melhorando a robustez a variações de iluminação e contraste.
+                              visualize=True,           # se a imagem HOG (uma representação visual das características HOG) deve ser retornada junto com o vetor de características.
+                              channel_axis=-1)          # índice do eixo do canal na imagem de entrada.
+        hog_features.append(features)
+        hog_images.append(img)
+    return np.array(hog_features), hog_images
 
 
-"""
-from sklearn.linear_model import LogisticRegression
-# Treinamento de um modelo Regressão Logistica
-def train(X_train, y_train):
-  model = LogisticRegression(max_iter=10000)
+# Criar e treinar o modelo Decision Tree
+def train(X_train, y_train, max_depth=None, min_samples_leaf=1):
+  model = DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf, random_state=42)
   model.fit(X_train, y_train)
   return model
 
-model = train(X_train_pca, y_train)
-X_train_pca.shape
+# Extraindo características HOG dos dados de treinamento e teste
+X_train_hog, train_hog_images = extract_hog_features(X_train)
+X_test_hog, _ = extract_hog_features(X_test)
 
-from sklearn.metrics import accuracy_score, cohen_kappa_score, f1_score, confusion_matrix
-import seaborn as sns
+print(X_train_hog.shape)
+
+n_components = 800
+pca = PCA(n_components=n_components)
+X_train_hog_pca = pca.fit_transform(X_train_hog)
+X_test_hog_pca = pca.transform(X_test_hog)
+
+print(X_train_hog_pca.shape)
+model = train(X_train_hog_pca, y_train)
+
+# Visualizar imagens HOG
+def visualize_multiple_hog(images, hog_images, images_to_show=5):
+    plt.figure(figsize=(6, images_to_show))
+
+    for i in range(images_to_show):
+        # Mostrar imagem original
+        plt.subplot(images_to_show, 2, 2 * i + 1)
+        plt.imshow(images[i], cmap='gray', vmin=0, vmax=1)
+        plt.title(f'Imagem Original {i+1}', fontsize=8)
+        plt.axis('off')
+
+        # Mostrar imagem HOG
+        plt.subplot(images_to_show, 2, 2 * i + 2)
+        plt.imshow(hog_images[i], cmap='gray')
+        plt.title(f'Imagem HOG {i+1}', fontsize=8)
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+# Exemplo de visualização das primeiras n imagens do conjunto de treinamento
+images_to_show = 5
+visualize_multiple_hog(X_train, train_hog_images, images_to_show=images_to_show)
+
 
 # Inferência e avaliação
 def predict_and_evaluate(model, X_test, y_test):
@@ -191,10 +200,6 @@ def predict_and_evaluate(model, X_test, y_test):
     plt.show()
 
 print('Resultados de Treino')
-predict_and_evaluate(model, X_train_pca, y_train)
-
+predict_and_evaluate(model, X_train_hog_pca, y_train)
 print('Resultados de Teste')
-predict_and_evaluate(model, X_test_pca, y_test)
-"""
-
-
+predict_and_evaluate(model, X_test_hog_pca, y_test)
