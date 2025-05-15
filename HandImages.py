4b4d8cde5481096cbdb458460013a12540f1fa53
RodrigoBuzzatti <rodrigo.buzzatti@gmail.com>
2025-05-15 18:40:37 -0300

    Inclusao do codigo para analisar outro Dataset: Handsimages

--- HandImages.py
+++ HandImages.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Função para carregar imagens de um diretório e redimensionar
-def load_folder(folder, img_size, labels_dict=None, max_images=None, sort=False):
+def load_folder(folder, img_size_width,img_size_hight, labels_dict=None, max_images=None, sort=False):
     print("Lendo arquivos na pasta: ",folder)
     images = []
     labels = []
@@ -18,7 +18,7 @@ def load_folder(folder, img_size, labels_dict=None, max_images=None, sort=False)
         file_list_inside_folder = os.listdir(well_formed_directory)
         for file_name in file_list_inside_folder:
             img_path = os.path.join(well_formed_directory, file_name)
-            img = Image.open(img_path).resize((img_size, img_size)).convert('RGB')
+            img = Image.open(img_path).resize((img_size_width,img_size_hight)).convert('RGB')
             img_array = np.array(img)
             images.append(img_array)
             count = count + 1
@@ -29,38 +29,14 @@ def load_folder(folder, img_size, labels_dict=None, max_images=None, sort=False)
     return np.array(images), np.array(labels)
 
 # Data set folder path: D:\rodri\Documents\OneDrive\Documentos\Cursos\Visual Computer Master\Trabalho Metodos Tradicionais\DataSet_HumanFaces
-img_size = 48  # Redimensionar imagens para 48x48 pixels
-train_folder = 'D:/rodri/Documents/OneDrive/Documentos/Cursos/Visual Computer Master/Trabalho Metodos Tradicionais/DataSet_HumanFaces/train'
-test_folder  = 'D:/rodri/Documents/OneDrive/Documentos/Cursos/Visual Computer Master/Trabalho Metodos Tradicionais/DataSet_HumanFaces/test'
+#                       D:\rodri\Documents\OneDrive\Documentos\Cursos\Visual Computer Master\Trabalho Metodos Tradicionais\DataSet_HandImages
+img_size_width = 348  # Redimensionar imagens para width x hight
+img_size_hight = 464
+dataset_folder = 'D:/rodri/Documents/OneDrive/Documentos/Cursos/Visual Computer Master/Trabalho Metodos Tradicionais/DataSet_HandImages'
 
-# Carregar imagens de treino
-X_train, y_train = load_folder(train_folder, img_size)
+# Carregar imagens
+X_train, y_train = load_folder(dataset_folder, img_size_width,img_size_hight)
 print('Shape do Treino: ',X_train.shape)
-
-# Carregar imagens de teste
-X_test, y_test = load_folder(test_folder, img_size)
-print('Shape do Teste: ',X_test.shape)
-
-"""
-# Função para detectar imagens corrompidas
-def detect_corrupted_images(folder):
-    corrupted_files = []
-    for filename in os.listdir(folder):
-        if filename.endswith(".jpg"):
-            try:
-                img = Image.open(os.path.join(folder, filename))
-                img.verify()  # Verificar a integridade do arquivo
-            except (IOError, SyntaxError) as e:
-                corrupted_files.append(filename)
-    return corrupted_files
-
-# Detectar imagens corrompidas
-corrupted_train_images = detect_corrupted_images(train_folder)
-print(f"Imagens corrompidas no conjunto de treino: {corrupted_train_images}")
-
-corrupted_submission_images = detect_corrupted_images(submission_folder)
-print(f"Imagens corrompidas no conjunto de submissão: {corrupted_submission_images}")
-"""
 
 import random
 
@@ -86,7 +62,6 @@ image_heights = X_train.shape[1] * np.ones(X_train.shape[0])
 image_widths = X_train.shape[2] * np.ones(X_train.shape[0])
 
 # Plotar valores mínimos e máximos dos pixels
-"""
 plt.figure(figsize=(12, 3))
 
 plt.subplot(1, 2, 1)
@@ -94,10 +69,8 @@ plt.hist(X_train.flatten(), bins=50, color='blue', alpha=0.7)
 plt.title(f'Valores de Pixels\nMínimo: {pixel_min}, Máximo: {pixel_max}')
 plt.xlabel('Valor do Pixel')
 plt.ylabel('Frequência')
-"""
 
 # Plotar gráfico de dispersão de largura vs altura
-"""
 plt.subplot(1, 2, 2)
 plt.scatter(image_widths, image_heights, alpha=0.5)
 plt.title(f'Largura vs Altura das Imagens\nAltura: Mínima: {image_heights.min()}, Máxima: {image_heights.max()}\nLargura: Mínima: {image_widths.min()}, Máxima: {image_widths.max()}')
@@ -107,7 +80,6 @@ plt.grid(True)
 
 plt.tight_layout()
 plt.show()
-"""
 
 # Normalizar os dados
 X_train = X_train.astype('float32') / 255.0
@@ -159,80 +131,4 @@ def train(X_train, y_train):
 
 model = train(X_train_pca, y_train)
 print(X_train_pca.shape)
-
-from sklearn.metrics import accuracy_score, cohen_kappa_score, f1_score, confusion_matrix
-import seaborn as sns
-
-# Inferência e avaliação
-def predict_and_evaluate(model, X_test, y_test):
-
-    # Inferência
-    y_pred = model.predict(X_test)
-    probabilities = model.predict_proba(X_test)
-
-    # Métricas
-    print('Acurácia:', accuracy_score(y_test, y_pred))
-    print('F1 score:', f1_score(y_test, y_pred, average='weighted'))
-
-    # Matriz de confusão
-    conf_matrix = confusion_matrix(y_test, y_pred)
-
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap="Blues", xticklabels=np.unique(y_test), yticklabels=np.unique(y_test))
-    plt.title('Matriz de Confusão')
-    plt.xlabel('Predito')
-    plt.ylabel('Verdadeiro')
-    plt.show()
-    return y_pred,probabilities
-
-print('Resultados de Treino')
-y_pred_treino,probabilities_treino = predict_and_evaluate(model, X_train_pca, y_train)
-
-print('Resultados de Teste')
-y_pred_teste,probabilities_teste = predict_and_evaluate(model, X_test_pca, y_test)
-
-# Analise dos Erros
-# Filtrar previsões incorretas
-incorrect_indices = np.where(y_pred_teste != y_test)[0]
-
-# Se houver previsões incorretas, selecione até 10 para exibir
-num_images_to_show = min(10, len(incorrect_indices))
-if num_images_to_show > 0:
-    plt.figure(figsize=(10, 4))
-    for i in range(num_images_to_show):
-        incorrect_index = incorrect_indices[i]
-        incorrect_image = X_test[incorrect_index]
-        true_label = y_test[incorrect_index]
-        predicted_label = y_pred_teste[incorrect_index]
-
-        plt.subplot(2, 5, i + 1)
-        plt.imshow(incorrect_image, cmap='gray')
-        plt.title(f'P:={predicted_label}, V:={true_label}', fontsize=10)
-        plt.axis('off')
-    plt.tight_layout()
-    plt.show()
-else:
-    print("Nenhuma previsão incorreta encontrada.")
-
-# Criar DataFrame com informações das previsões incorretas
-incorrect_predictions = []
-
-for i in incorrect_indices:
-    true_label = y_test[i]
-    predicted_label = y_pred_teste[i]
-    row = {
-        'indice': i,
-        'true': true_label,
-        'pred': predicted_label
-    }
-    # Adicionar as probabilidades para cada classe
-    for class_index in range(7):
-        row[f'proba_{class_index}'] = probabilities_teste[i, class_index]
-    incorrect_predictions.append(row)
-
-df_incorrect_predictions = pd.DataFrame(incorrect_predictions)
-
-# Exibir o DataFrame
-print(df_incorrect_predictions.head(10))
-
-
 
